5e12dcf47735120488ef8d8c35983283978b16d6
Егор Плуталов <rgor0204@gmail.com>
2021-05-20 21:34:07 +0300

    Revert "Revert "Выполнен код для задания 1""
    This reverts commit c4665ede9e941a1eff503ff1ad7ad3d5df208ece.

---
+++ 1/Task_1/Task 1-1/main.cpp
@@ -0,0 +1,51 @@
+using namespace std;
+
+#define _USE_MATH_DEFINES
+
+#include <cmath>
+#include <iostream>
+#include "main.h"
+
+/**
+ * @brief Функция нахождения  математического выражения.
+ * @param x Первая перменная.
+ * @param y Вторая переменная.
+ * @param z Третья переменная.
+ * @return Bозвращает значение а.
+*/
+double FirstPrimer(double x, double y, double z)
+{
+    double a = (pow(z, 2) * x + pow(M_E, -x) * cos(y * x)) / (y * x - pow(M_E, -x) * sin(y * x) + 1);
+    return a;
+}
+
+
+/**
+ * @brief Функция нахождения  математического выражения.
+ * @param x Первая перменная.
+ * @param y Вторая переменная.
+ * @param z Третья переменная.
+ * @return Bозвращает значение b.
+*/
+double SecondPrimer(double x, double y, double z)
+{
+    double b = pow(M_E, 2 * x) * log(z + x) - pow(y, 3 * x) * log(y - x);
+    return b;
+}
+
+/**
+ * @brief Точка входа в программу.
+ * @return Возвращает нуль в  случе успеха.
+*/
+int main()
+{
+    double FirstVariable, SecondVariable, ThirdVariable;
+    FirstVariable = 0.3;
+    SecondVariable = 2.9;
+    ThirdVariable = 0.5;
+
+    cout << FirstPrimer(FirstVariable, SecondVariable, ThirdVariable) << '\n';
+    cout << SecondPrimer(FirstVariable, SecondVariable, ThirdVariable);
+
+    return 0;
+}
