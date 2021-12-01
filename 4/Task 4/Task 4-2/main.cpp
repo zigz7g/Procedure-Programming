dc5d92f04e1adb6aa12786dbb72e2e590709180c
Егор Плуталов <44480792+zigz7g@users.noreply.github.com>
2021-12-01 12:12:14 +0300

    Изменение кода

--- 4/Task 4/Task 4-2/main.cpp
+++ 4/Task 4/Task 4-2/main.cpp
@@ -4,34 +4,51 @@ using namespace std;
 #include <cstdlib>
 #include <ctime>
 #include <cmath>
-
-/// @brief Функция заполнения массива
-/// @param arr Массив
-
-void FillArray(int arr[]);
-
-/// @brief Замена последнего элемента массива кратного 3 нулем
-/// @param arr Массив
-
-void FirstTask(int arr[]);
-
-/// @brief Функция для вставки заданного числа после каждого четного элемента
-/// @param arr Массив
-/// @param K Заданное число
-
-void SecondTask(int arr[], int K);
-
-/// @brief Функция формировки нового массива
-/// @param P Массив
-
-void ThirdTask(int P[]);
+#include <conio.h>
 
 
-void FillArray(int arr[])
+/// @brief Функция заполнения массива рандомными числами.
+/// @param arr Массив.
+/// @param n Размерность массива.
+
+void FillArray(int* arr, const int n);
+
+/// @brief Функция заполненеия массива с клавиатуры.
+/// @param arr Массив.
+/// @param n Размерность массива.
+
+void FillArrayFromKeybord(int* arr, const int n);
+
+/// @brief Функция вывода меню.
+
+void prnmn();
+
+/// @brief Функция для замены полседнего элемента кратного 3 нулем.
+/// @param arr Массив.
+/// @param n Размер массива.
+
+void FirstTask(int* arr, const int n);
+
+/// @brief Функция для вставки числа К после каждого четного элемента.
+/// @param arr Массив.
+/// @param K Заданное число.
+/// @param n Размерность массива.
+
+void SecondTask(int* arr, int K, const int n);
+
+/// @brief Функция для создания нового массива на основе заданного с той же размерностью.
+/// @param P Массив.
+/// @param n Размерность массива.
+
+void ThirdTask(int* P, const int n);
+
+
+
+void FillArray(int* arr, const int n)
 {
 
 	srand(time(NULL));
-	for (int i = 0; i <= 199; i++)
+	for (int i = 0; i <= n -1; i++)
 	{
 		arr[i] = rand() % 201 - 100;
 		cout.width(3);
@@ -39,18 +56,39 @@ void FillArray(int arr[])
 	}
 }
 
-
-
-void FirstTask(int arr[])
+void FillArrayFromKeybord(int* arr, const int n)
 {
-	cout << '\n'<< '\n' << "Замена последнего элемента" << '\n';
+	for (int i = 0; i <= n; i++)
+	{
+		cin >> arr[i];
+		cout.width(3);
+		cout << arr[i] << ", ";
+	}
+}
 
-	for (int i = 199; i >= 0; i--)
+
+void prnmn()
+{
+	cout << "\nMENU";
+	cout << "\n1-Замена последнего элемента кратного 3 нулем.";
+	cout << "\n2-Вставить элемент со значением К после всех нечетных элементов, начинающихся на цифру К.";
+	cout << "\n3-Из элементов массива P сформировать массив M той же размерности.";
+	cout << "\nESC-Вход";
+}
+
+
+
+
+void FirstTask(int* arr, const int n)
+{
+	cout << '\n' << '\n' << "Замена последнего элемента" << '\n';
+
+	for (int i = n - 1; i >= 0; i--)
 	{
 		if (arr[i] % 3 == 0)
 		{
 			arr[i] = 0;
-			cout <<'\n' << "Последний элемент, который заменили - " << i << '\n';
+			cout << '\n' << "Последний элемент, который заменили - " << i << '\n';
 			cout << '\n' << "Теперь этот элемент равен = " << arr[i] << '\n';
 			break;
 		}
@@ -61,11 +99,11 @@ void FirstTask(int arr[])
 
 
 
-void SecondTask(int arr[], int K)
+void SecondTask(int* arr, int K, const int n)
 {
 	cout << '\n' << '\n' << "Вставка заданного числа" << '\n';
 
-	for (int i = 0; i <= 199; i++)
+	for (int i = 0; i <= n - 1; i++)
 	{
 		if (arr[i] % 2 == 0)
 		{
@@ -74,7 +112,7 @@ void SecondTask(int arr[], int K)
 		}
 
 	}
-	for (int i = 0; i <= 199; i++)
+	for (int i = 0; i <= n - 1; i++)
 	{
 		cout.width(3);
 		cout << arr[i] << ", ";
@@ -83,13 +121,14 @@ void SecondTask(int arr[], int K)
 
 
 
-void ThirdTask(int P[])
+void ThirdTask(int* P, const int n)
 {
 	cout << '\n' << '\n' << "Формировка нового массива" << '\n';
 
-	long int M[200];
+	int* M;
+	M = new int[n];
 
-	for (int i = 0; i <= 199; i++)
+	for (int i = 0; i <= n -1; i++)
 	{
 		if (i % 4 == 0 && i != 0)
 		{
@@ -99,7 +138,7 @@ void ThirdTask(int P[])
 		{
 			M[i] = 0;
 		}
-		else if (i == 199)
+		else if (i == n - 1)
 		{
 			M[i] = 0;
 		}
@@ -114,18 +153,58 @@ void ThirdTask(int P[])
 }
 
 
-
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 
-	int mas[200], Value;
+	int Value, n;
+	cout << "Введите кол-во элементов массива" << '\n';
+	cin >> n;
+	int* mas;
+	mas = new int[n];
+	cout << "Введите заданное число" << '\n';
 	cin >> Value;
+	char key = ' ';
 
-	FillArray(mas);
-	FirstTask(mas);
-	SecondTask(mas, Value);
-	ThirdTask(mas);
+	cout << "Выберете способ заполнения массива" << '\n';
+	cout << "1.Рандом" << '\n';
+	cout << "2.Вручную" << '\n';
 
+	key = _getch();
+
+	if (key == '1')
+	{
+		FillArray(mas, n);
+	}
+	else
+	{
+		FillArrayFromKeybord(mas, n);
+	}
+
+
+
+	while (key != 27)
+	{
+		prnmn();
+		key = _getch();
+
+		switch (key)
+		{
+		case '1':
+			FirstTask(mas, n);
+			break;
+		case '2':
+			SecondTask(mas, Value, n);
+			break;
+		case '3':
+			ThirdTask(mas,n);
+			break;
+		}
+		
+
+
+		key = _getch();
+		system("cls");
+	}
 	return 0;
 }
