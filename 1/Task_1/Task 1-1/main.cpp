de3ee1acdd8c050841699bb19354111f2aefc2ef
Егор Плуталов <44480792+zigz7g@users.noreply.github.com>
2021-12-06 19:54:22 +0300

    Update main.cpp

--- 1/Task_1/Task 1-1/main.cpp
+++ 1/Task_1/Task 1-1/main.cpp
@@ -6,25 +6,33 @@ using namespace std;
 #include <iostream>
 #include <locale>
 
-/// @brief Функция для расчёта первого примера.
-/// @param x Переменная х.
-/// @param y Переменная у.
-/// @param z Переменная z.
-/// @return Возвращает решение данного примера.
+/**
+ * \brief Функция для расчёта первого примера.
+ * \param x Переменная х. 
+ * \param y Переменная у.
+ * \param z Переменная z.
+ * \return Возвращает решение данного примера.
+*/
 
-double FirstPrimer(double x, double y, double z);
+double FirstEquation(const double x, const double y, const double z);
 
-/// @brief Функция для расчёта второго примера.
-/// @param x Переменная х.
-/// @param y Переменная у.
-/// @param z Переменная z.
-/// @return Возвращает решение данного примера.
 
-double SecondPrimer(double x, double y, double z);
 
-/// @brief Точка входа в программу.
-/// @return Возвращает 0 в случае успеха.
- 
+/**
+ * \brief Функция для расчёта второго примера.
+ * \param y Переменная у.
+ * \param x Переменная х.
+ * \param z Переменная z.
+ * \return Возвращает решение данного примера.
+*/
+
+double SecondEquation(const double x, const double y, const double z);
+
+/**
+ * \brief Точка входа в программу.
+ * \return Возвращает 0 в случае успеха.
+*/
+
 int main()
 {
     setlocale(LC_ALL, "Russian");
@@ -34,22 +42,22 @@ int main()
     SecondVariable = 2.9;
     ThirdVariable = 0.5;
 
-    cout << FirstPrimer(FirstVariable, SecondVariable, ThirdVariable) << '\n';
-    cout << SecondPrimer(FirstVariable, SecondVariable, ThirdVariable);
+    cout << FirstEquation(FirstVariable, SecondVariable, ThirdVariable) << '\n';
+    cout << SecondEquation(FirstVariable, SecondVariable, ThirdVariable);
 
     return 0;
 }
 
-double FirstPrimer(double x, double y, double z)
+double FirstEquation(const double x, const double y, const double z)
 {
-    double a = (pow(z, 2) * x + pow(M_E, -x) * cos(y * x)) / (y * x - pow(M_E, -x) * sin(y * x) + 1);
+    double a = (pow(z, 2) * x + exp(-x) * cos(y * x)) / (y * x - exp(-x) * sin(y * x) + 1);
     return a;
 }
 
 
 
-double SecondPrimer(double x, double y, double z)
+double SecondEquation(const double x, const double y, const double z)
 {
-    double b = pow(M_E, 2 * x) * log(z + x) - pow(y, 3 * x) * log(y - x);
+    double b = exp(2 * x) * log(z + x) - pow(y, 3 * x) * log(y - x);
     return b;
 }
